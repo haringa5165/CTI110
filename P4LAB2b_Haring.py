ca0cef2acc474e8880c9dd1194fbdd3600da0722
haringa5165 <98965656+haringa5165@users.noreply.github.com>
2022-03-31 12:07:09 -0400

    Add files via upload
    Turtle Graphic Initials

---
+++ P4LAB2b_Haring.py
@@ -0,0 +1,33 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+my_turtle.color('purple')
+my_turtle.pensize(4)
+
+my_turtle.left(78)
+my_turtle.forward(74)
+my_turtle.right(156)
+my_turtle.forward(37)
+my_turtle.right(102)
+my_turtle.forward(16)
+my_turtle.backward(16)
+my_turtle.left(102)
+my_turtle.forward(37)
+my_turtle.penup()
+my_turtle.left(78)
+my_turtle.forward(20)
+my_turtle.pendown()
+
+my_turtle.left(90)
+my_turtle.forward(70)
+my_turtle.backward(35)
+my_turtle.right(90)
+my_turtle.forward(20)
+my_turtle.left(90)
+my_turtle.forward(35)
+my_turtle.backward(70)
+my_turtle.penup()
+
+
+
+
